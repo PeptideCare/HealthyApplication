9aa84699125fdad2b305cdc4321ce63cdebc3aab
DESKTOP-86N6AHS\user <wldns997@naver.com>
2021-11-30 16:18:16 +0900

    21.11.30
    필드값 오류 수정

--- healthyapplication/MainActivity.java
+++ healthyapplication/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     String imgName;
     Long titleId;
     String titleName = "";
+    String sex;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -143,17 +144,56 @@ public class MainActivity extends AppCompatActivity {
                             titleName = obj.getString("name");
                         }
 
+                        // 회원 성별 추출
+                        sex = jsonObject.getString("sex");
+
+                        // UI 변경
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                name.setText(imgName);
-                                title.setText(titleName);
-                                Glide.with(getApplicationContext()).load(R.raw.man).into(img);
-                                if (imgName.equals("0")) {
-                                    name.setText("평범한 사람");
-                                }
-                                if (titleName == null) {
+
+//                                // 기본 세팅
+//                                if (imgName.equals("0")) {
+//                                    if (sex.equals("남")) {
+//                                        name.setText("평범한 남자");
+//                                        Glide.with(getApplicationContext()).load(R.raw.normal_man).into(img);
+//                                    } else {
+//                                        name.setText("평범한 여자");
+//                                        Glide.with(getApplicationContext()).load(R.raw.normal_woman).into(img);
+//                                    }
+//                                }
+
+                                // 필드값 별로 세팅
+                                    name.setText(imgName);
+                                    if (sex.equals("남")) {
+                                        if (imgName.equals("보디빌더")) {
+                                            Glide.with(getApplicationContext()).load(R.raw.body_man).into(img);
+                                        } else if (imgName.equals("트레이너")){
+                                            Glide.with(getApplicationContext()).load(R.raw.trainer_man).into(img);
+                                        } else if (imgName.equals("모델")) {
+                                            Glide.with(getApplicationContext()).load(R.raw.model_man).into(img);
+                                        } else {
+                                            name.setText("평범한 남자");
+                                            Glide.with(getApplicationContext()).load(R.raw.normal_man).into(img);
+                                        }
+                                    } else {
+                                        if (imgName.equals("보디빌더")) {
+                                            Glide.with(getApplicationContext()).load(R.raw.body_woman).into(img);
+                                        } else if (imgName.equals("트레이너")){
+                                            Glide.with(getApplicationContext()).load(R.raw.trainer_woman).into(img);
+                                        } else if (imgName.equals("모델")) {
+                                            Glide.with(getApplicationContext()).load(R.raw.model_woman).into(img);
+                                        } else {
+                                            name.setText("평범한 여자");
+                                            Glide.with(getApplicationContext()).load(R.raw.normal_woman).into(img);
+                                        }
+                                    }
+
+                                // 칭호 세팅
+                                if (titleName.length() == 0) {
                                     title.setText("평범함");
+                                } else {
+                                    title.setText(titleName);
                                 }
                             }
                         });
--- service/FieldService.java
+++ service/FieldService.java
@@ -31,70 +31,18 @@ public class FieldService {
         // 칭호 이벤트 처리
         if (findField != null){
             findField.addTime(field.getHour());
-            if (findField.getName().equals("공부")) {
-                if (findField.getHour() >= 10000) {
-                    Image image = new Image();
-                    image.setName("대통령");
-                    imageService.update(image, id);
-
-                    Title title = new Title();
-                    title.setName("공부의 신");
-                    titleService.save(title, id);
-
-                } else if (findField.getHour() >= 1000) {
-                    Image image = new Image();
-                    image.setName("국회의원");
-                    imageService.update(image, id);
-
-                    Title title = new Title();
-                    title.setName("공부는 나의길");
-                    titleService.save(title, id);
-                } else if (findField.getHour() >= 100) {
-                    Image image = new Image();
-                    image.setName("공무원");
-                    imageService.update(image, id);
-
-                    Title title = new Title();
-                    title.setName("공부 꿈나무");
-                    titleService.save(title, id);
-                }
-            }
-
-            if (findField.getName().equals("운동")) {
-                if (findField.getHour() >= 10000) {
-                    Image image = new Image();
-                    image.setName("보디빌더");
-                    imageService.update(image, id);
-
-                    Title title = new Title();
-                    title.setName("운동의 신");
-                    titleService.save(title, id);
-                } else if (findField.getHour() >= 1000) {
-                    Image image = new Image();
-                    image.setName("스포츠 모델");
-                    imageService.update(image, id);
-
-                    Title title = new Title();
-                    title.setName("운동은 나의길");
-                    titleService.save(title, id);
-                } else if (findField.getHour() >= 100) {
-                    Image image = new Image();
-                    image.setName("몸짱");
-                    imageService.update(image, id);
-
-                    Title title = new Title();
-                    title.setName("운동 꿈나무");
-                    titleService.save(title, id);
-                }
-            }
+            checkField(id, findField, findField.getHour()+field.getHour());
 
         } else {
             Optional<Member> findMember = memberRepository.findById(id);
             if (findMember.isEmpty()) return;
             field.setMember(findMember.get());
+
             fieldRepository.save(field);
+            checkField(id, field, field.getHour());
         }
     }
+
 
     //삭제
     @Transactional
@@ -112,5 +60,65 @@ public class FieldService {
     public List<Field> findAllById(String id) {
         List<Field> fields = fieldRepository.findAllById(id);
         return fields;
+    }
+
+    // 필드 시간 별 이미지 칭호 저장
+    private void checkField(String id, Field findField, int hour) {
+        if (findField.getName().equals("공부")) {
+            if (hour >= 10000) {
+                Image image = new Image();
+                image.setName("Trillionaire");
+                imageService.update(image, id);
+
+                Title title = new Title();
+                title.setName("공부의 神");
+                titleService.save(title, id);
+
+            } else if (hour >= 1000) {
+                Image image = new Image();
+                image.setName("Billionaire");
+                imageService.update(image, id);
+
+                Title title = new Title();
+                title.setName("공부는 나의길");
+                titleService.save(title, id);
+            } else if (hour >= 100) {
+                Image image = new Image();
+                image.setName("Millionaire");
+                imageService.update(image, id);
+
+                Title title = new Title();
+                title.setName("공린이");
+                titleService.save(title, id);
+            }
+        }
+
+        if (findField.getName().equals("운동")) {
+            if (hour >= 10000) {
+                Image image = new Image();
+                image.setName("보디빌더");
+                imageService.update(image, id);
+
+                Title title = new Title();
+                title.setName("운동의 神");
+                titleService.save(title, id);
+            } else if (hour >= 1000) {
+                Image image = new Image();
+                image.setName("트레이너");
+                imageService.update(image, id);
+
+                Title title = new Title();
+                title.setName("운동은 나의길");
+                titleService.save(title, id);
+            } else if (hour >= 100) {
+                Image image = new Image();
+                image.setName("모델");
+                imageService.update(image, id);
+
+                Title title = new Title();
+                title.setName("헬린이");
+                titleService.save(title, id);
+            }
+        }
     }
 }
